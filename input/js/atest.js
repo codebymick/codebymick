926241e31c661cc14235bf55adb1c1ea2d2eac31
codebymick <codebymick@gmail.com>
2017-12-05 17:07:05 +0100

    js linting and minification

---
+++ input/js/atest.js
@@ -0,0 +1,8 @@
+/**
+* second Parallax.js
+* @author Matthew Wagerfield - @wagerfield, René Roth - mail@reneroth.org
+* @description Creates a parallax effect between an array of layers,
+*              driving the motion from the gyroscope output of a smartdevice.
+*              If no gyroscope is available, the cursor position is used.
+*/
+
---
+++ output/js/script.js
@@ -0,0 +1,31 @@
+/**
+* BLAH BLAH BLAH
+* @author Matthew Wagerfield - @wagerfield, René Roth - mail@reneroth.org
+* @description Creates a parallax effect between an array of layers,
+*              driving the motion from the gyroscope output of a smartdevice.
+*              If no gyroscope is available, the cursor position is used.
+*/
+
+
+/**
+* second Parallax.js
+* @author Matthew Wagerfield - @wagerfield, René Roth - mail@reneroth.org
+* @description Creates a parallax effect between an array of layers,
+*              driving the motion from the gyroscope output of a smartdevice.
+*              If no gyroscope is available, the cursor position is used.
+*/
+
+
+/**
+* Parallax.js
+* @author Matthew Wagerfield - @wagerfield, René Roth - mail@reneroth.org
+* @description Creates a parallax effect between an array of layers,
+*              driving the motion from the gyroscope output of a smartdevice.
+*              If no gyroscope is available, the cursor position is used.
+*/
+
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+document.getElementById("demo").innerHTML = z;
